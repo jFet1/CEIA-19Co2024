44cf9a6500a5c5aefc4199b6b4233eb215aebb36
jFet1 <ojedajuancz@gmail.com>
2025-03-29 20:32:47 -0300

    Agrego archivos del 2do bimestre - materia AdD

---
+++ 2 Bimestre/AdD/actualizar-profes.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "E:\IA\2 Bimestre\AdD\CEIA_Analisis_de_datos"
+echo ============================
+echo Actualizando repo de docentes...
+git pull
+echo ============================
+pause
