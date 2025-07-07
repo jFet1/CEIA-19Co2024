3eaa3998b6b51a2afde3435a681ebc62c5195bca
jFet1 <ojedajuancz@gmail.com>
2025-07-07 11:52:32 -0300

    fix git

--- 2 Bimestre/AdD/actualizar-profes.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-C:\Users\Juan\Documents\IA\CEIA-19Co2024\2 Bimestre\AdD\CEIA_Analisis_de_datos"
-echo ============================
-echo Actualizando repo de docentes...
-git pull
-echo ============================
-pause
